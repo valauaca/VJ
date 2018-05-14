19d480cc1d10ec02afeee82a7ac329fa885e6a3d
valauaca <37352292+valauaca@users.noreply.github.com>
2018-05-14 16:07:08 -0500

    Add files via upload

--- ValentinaJimenez_taller5/punto_1/grafica.py
+++ ValentinaJimenez_taller5/punto_1/grafica.py
@@ -1,8 +1,9 @@
 import numpy as np
+from numpy import *
 
 import matplotlib.pyplot as plt
 
-
+"""
 #cargar los datos para la grafica
 
 datos=np.loadtxt('placas.txt')
@@ -24,4 +25,26 @@ c,v=np.meshgrid(x,y)
 plt.streamplot(a,b,c,v)
 
 plt.imshow(datos)
-plt.savefig('placas.pdf')
+plt.savefig('placas.pdf')"""
+from numpy import *
+import matplotlib.pyplot as plt
+
+
+#metodo tomado de : para usar contourf https://matplotlib.org/api/_as_gen/matplotlib.pyplot.contourf.html. Este metodo lo implemente en el laboratorio con ayuda del profesor desde la pagina anteriormente citada. 
+
+
+datos = genfromtxt("datos.txt", delimiter=',')
+X = ~any(isnan(datos),0)
+datos = datos[:,X] 
+v = int(size(datos,0)/3)
+r=linspace(0,v,v)
+h=linspace(0,v,v)
+X,Y = meshgrid(r,h)
+
+
+figura = plt.figure()
+plt.contourf(X,Y,datos[:v,:], 40, cmap='ocean_r')
+plt.colorbar()
+plt.streamplot(X,Y,datos[v:2*v,:],datos[2*v:,:], 1.1, color="navy")
+
+figura.savefig('placas.pdf')
--- ValentinaJimenez_taller5/punto_1/placas.cpp
+++ ValentinaJimenez_taller5/punto_1/placas.cpp
@@ -1,205 +1,174 @@
-#include <iostream>
-#include <math.h>
 #include "iostream"
 #include "fstream"
-#include "math.h"
+
 
 using namespace std;
 
+int main()
+{
 
-// declarar conste
+// declarar constantes
 
-int n=511;
-double placas[511][511];
-float v[511][511];
-float v1[511][511];
-float L= 5.0;
-float h=5.0/512.0;
+	int** placas= new int *[512];
 
+	for (int i=0; i<512; i++){
 
-
-
-// inicializa funciones
-void iniciar(){
-
-	
-	for( int i =0; i<n; i++){
-
-		for (int j= 0; j<n; j++){
-			placas[i][j]=0;
-
-
-		}
-
-
-
+		placas[i]=new int[512];
 	}
 
-	// condici{on inicial
-	for( int i =0; i<n; i++)
+
+	double** V_inicial= new double *[512];
+
+	for (int i=0; i<512; i++){
+
+		V_inicial[i]=new double[512];
+	}
+
+
+	double** V_final= new double *[512];
+
+	for (int i=0; i<512; i++){
+
+		V_final[i]=new double[512];
+	}
+
+	double L = 5.0;
+	float d = 1.0;
+	float l = 2.0;
+	double h = 0.009765625;
+	int n=511;
+
+// inicializar las matrices
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			placas[i][j] = 0; 
+		}
+	}
+
+// condiciones iniciales
+
+	for (int i = 0; i < n; ++i)
 	{
 
-		placas[i][0]=2;
-		placas[i][n-1]=2;
-		placas[0][i]=2;
-		placas[n-1][0]=2;
-
-
-
+		placas[0][i] = 2;
+		placas[n-1][0] = 2;
+		placas[i][0] = 2; 
+		placas[i][n-1] = 2; 
+ 
 	}
-
-	float d=1.0;
-	float r=2.0;
-	int c= (r/L);
-	int w= (d/L);
-	int k=n*((0.5)-(0.5)*c);
-	int f=n*c;
-	int h0= n*((0.5)-(0.5)*w);
-	int hf= n*(w);
-
-	for (int i=k; i< k+f; i++){
-
-		placas[h0][i]=1;
-		placas[h0+hf][i]=-1;
-		
-
-	}
-
-	for (int i=0; i<n; i++){
-
-		for(int j=0; j<n; j++){
-
-			if(placas[i][j]==0 | placas[i][j]==2){
-
-
-				v[i][j]=0.0;
-			}
-
-			else if (placas[i][j]==1){
-
-				v[i][j]=50.0;
-			}
-			else if(placas[i][j]==-1){
-
-				v[i][j]=-50.0;
-			}
-
-			v1[i][j]=v[i][j];
-
-		}
-
-
-	}
-
-
-}
-
-void actualizar(){
-
-
-	for (int i=0; i<n; i++){
-
-		for(int j=0; j<n;j++){
-
-			if(placas[i][j]==0){
-				v1[i][j]=v[i][j+1]+v[i][j-1]+v[i+1][j]+v[i-1][j];
-				v1[i][j]=v1[i][j]/4.0;
-			}
-
-		}
-	}
-
-	for(int i=0; i<n; i++){
-
-		for (int j=0; j<n;i++){
-
-			v[i][j]=v1[i][j];
-		}
-	}
-
-
-}
-
-
-
 
 	
 
-void imprimir_datos(){
+	int j0 = n*((0.5)-l/(2*L));
+	int Dj = n*(l/L);
+
+	int i0 = n*((0.5)-d/(2*L));
+	int Di = n*(d/L);
+
+	for (int j = j0; j < j0+Dj; ++j)
+	{
+		placas[i0][j] = 1;
+		placas[i0+Di][j] = -1;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			if(placas[i][j]==0 | placas[i][j]==2)
+			{ 
+				V_inicial[i][j] = 0.0;
+			}
+			else if (placas[i][j]==1)
+			{
+				V_inicial[i][j] = 50.0;
+			}
+			else if (placas[i][j]==-1)
+			{
+				V_inicial[i][j] = -50.0;
+			}
+
+			V_final[i][j] = V_inicial[i][j];
+		}
+	}
+
+// actualiza y suavizado
+
+	int u= int(0.01*2*n*n);
+
+	for (int k = 0; k < u; ++k)
+	{
+		for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			if(placas[i][j]==0)
+			{ 
+				V_final[i][j] = V_inicial[i][j+1] + V_inicial[i][j-1] + V_inicial[i+1][j] + V_inicial[i-1][j];
+				V_final[i][j] = V_final[i][j]/4.0;
+			}
+		}
+
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			V_inicial[i][j] = V_final[i][j];
+		}
+
+	}
+	}
+
+// inicializa campo electrico
 
 	double campo_E[511][511][2];
 
-	ofstream datos_placas("datos_placas.txt");
 
-	for (int i =0; i<n; i++){
+// imprimir los datos 
 
-		for (int j=0; j< n; j++){
+	ofstream datos("datos.txt"); // para enviar los datos a un archivo .txt
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			datos << V_inicial[i][j] << ",";
 
-			datos_placas << v[i][j] << ",";
-
-			if(placas[i][j]!=2){
-
-
-				campo_E[i][j][0]=(-1)*(v1[i][j]-v1[i][j-1])/h;
-				campo_E[i][j][1]=(-1)*(v1[i][j]-v1[i-1][j])/h;
-
-			}
-			else{
-				campo_E[i][j][0]=0.0;
-				campo_E[i][j][1]=0.0;
-
-			}
-
-				
+			if (placas[i][j]!=2)
+			{ 
+				campo_E[i][j][0] = -( V_final[i][j]-V_final[i][j-1] )/h;
+				campo_E[i][j][1] = -( V_final[i][j]-V_final[i-1][j] )/h;
+			}else
+			{
+				campo_E[i][j][0] = 0.0;
+				campo_E[i][j][1] = 0.0;
+			}	
 		}
-		datos_placas<< endl;
+		datos << endl;
 	}
-
-	for(int i=0; i<n; i++){
-
-		for(int j=0; j<n; j++){
-
-			datos_placas << campo_E[i][j][0] << ",";
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			datos << campo_E[i][j][0] << ",";
 		}
-		datos_placas << endl;
-
+		datos << endl;
 	}
-
-	for (int i=0; i < n; i++){
-
-		for (int j=0; j<n; j++){
-
-			datos_placas << campo_E[i][j][1] << ",";
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			datos << campo_E[i][j][1] << ",";
 		}
-		datos_placas << endl;
+		datos << endl;
 	}
-
-
-	
-}
-
-
-
-int main(){
-
-
-
-	iniciar();
-
-	int nt= int(0.01*2*n*n); 
-
-
-	for (int i=0; i< nt; i++){
-
-		actualizar();
-	}
-
-	
-
-	imprimir_datos();
 
 	return 0;
-
 }
+
+
 
 
